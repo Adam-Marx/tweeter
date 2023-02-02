789277be8379538c1f2ed421e2163da7514a3024
Adam Marx <adamalexmarx@gmail.com>
2023-02-01 18:32:08 -0800

    Implement get and post request using ajax and promises, implement renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,76 +7,114 @@
 $(document).ready( function () {
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const renderTweets = (tweets) => {
-  for (const tweet of tweets) {
-    const $returnValue = createTweetElement(tweet)
-    $('#tweets-container').append($returnValue)
-  }
-};
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      const $returnValue = createTweetElement(tweet)
+      $('#tweets-container').append($returnValue)
+    }
+  };
 
-const createTweetElement = (tweet) => {
-  const currentDate = new Date();
-  const tweetDate = new Date(tweet.created_at);
-  const timeDiff = currentDate - tweetDate;
-  const diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const createTweetElement = (tweet) => {
+    const currentDate = new Date();
+    const tweetDate = new Date(tweet.created_at);
+    const timeDiff = currentDate - tweetDate;
+    const diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
-  let dateString;
-  if (diffDays === 0) {
-    dateString = 'Today';
-  } else if (diffDays === 1) {
-    dateString = 'Yesterday';
-  } else {
-    dateString = `${diffDays} days ago`;
-  }
+    let dateString;
+    if (diffDays === 0) {
+      dateString = 'Today';
+    } else if (diffDays === 1) {
+      dateString = 'Yesterday';
+    } else {
+      dateString = `${diffDays} days ago`;
+    }
 
-  const $tweet = $(`
-  <article id="tweet">
+    const $tweet = $(`
+    <article id="tweet">
 
-  <header class="tweets-headings">
-    <h4>${tweet.user.name}</h4>
-    <h4 id="username">${tweet.user.handle}</h4>
-  </header>
+    <header class="tweets-headings">
+      <h4>${tweet.user.name}</h4>
+      <h4 id="username">${tweet.user.handle}</h4>
+    </header>
 
-  <div class="tweet-content">
-  <span>${tweet.content.text}</span>
-  </div>
+    <div class="tweet-content">
+    <span>${tweet.content.text}</span>
+    </div>
 
-  <footer>
-    <span id="date">${dateString}</span>
-    <span id="icons"><i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i></span>
-  </footer>
+    <footer>
+      <span id="date">${dateString}</span>
+      <span id="icons"><i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i></span>
+    </footer>
 
-  </article>
-  `);
+    </article>
+    `);
 
-  return $tweet;
-};
+    return $tweet;
+  };
 
-renderTweets(data);
+  renderTweets(data);
+
+  const loadTweets = () => {
+
+    $.ajax({
+      url: '/tweets',
+      type: "GET",
+      dataType: 'json',
+    })
+    .then(function(tweets) {
+      console.log('Success:', tweets);
+      renderTweets(tweets);
+    })
+    .catch(function(err) {
+      console.error('Error fetching tweets:', err)
+    });
+    };
+
+  loadTweets();
+
+  $('#new-tweet-form').submit(function(e) {
+    e.preventDefault();
+
+    const tweetData = $(this).serialize();
+
+    $.ajax({
+      url: '/tweets',
+      type: "POST",
+      data: tweetData
+    })
+    .then(function(res) {
+      console.log('Tweet sent successfully:', res);
+    })
+    .catch(function(err) {
+      console.error('Error sending tweet to server:', err);
+    });  
+  });
 
 });
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-  console.log('Testing jQuery');
-});
 
-$('#tweet-text').on("input", function() {
-  const $textarea = $(this);
-  const text = $textarea.val();
-  const remaining = 140 - text.length;
-  const form = $textarea.closest('form');
-  const counter = form.find('.counter');
-  counter.text(remaining);
-  
-  if (remaining < 0) {
-    counter.addClass('red') 
-  } else {
-    counter.removeClass('red')
-  }
 
+  $('#tweet-text').on("input", function() {
+    const $textarea = $(this);
+    const text = $textarea.val();
+    const remaining = 140 - text.length;
+    const form = $textarea.closest('form');
+    const counter = form.find('.counter');
+    counter.text(remaining);
+    
+    if (remaining < 0) {
+      counter.addClass('red') 
+    } else {
+      counter.removeClass('red')
+    }
+
+  });
 });
